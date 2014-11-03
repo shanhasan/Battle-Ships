e87f7adb976f207f2d48db7d5a740bf859cf34d7
ed <ejbyne@gmail.com>
2014-11-03 17:56:08 +0000

    Added knows which ship it is holding test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,14 @@
 class Cell
 
-def initialize
-  @has_ship = false
-end
+  def initialize
+    @has_ship = false
+  end
 
-attr_reader :has_ship
+  attr_reader :has_ship
+
+  def hold(ship)
+    @has_ship = true
+  end
+
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,6 +3,7 @@ require './lib/cell'
 describe Cell do
 
   let (:cell) { Cell.new }
+  let (:ship) { double :ship }
 
   it 'will initialize without a ship' do
     expect(cell.has_ship).to be false
@@ -11,6 +12,11 @@ describe Cell do
   it 'can hold a ship' do
     cell.hold(ship)
     expect(cell.has_ship).to be true
+  end
+
+  xit 'knows which ship it is holding' do
+    cell.hold(ship)
+    expect(cell.ship).to be(ship)
   end
   
 end
