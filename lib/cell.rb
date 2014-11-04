208c8740c6c8acc55226163ff375c92436c878fa
ed <ejbyne@gmail.com>
2014-11-04 11:59:22 +0000

    Amended Cell class to test for water on initializing

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,22 +3,23 @@ class Cell
   attr_accessor :empty, :ship, :hit
   attr_reader :coordinates
 
-  def initialize(coordinates)
-    @coordinates = coordinates.fetch(:coordinates)
+  def initialize #(coordinates)
+    # @coordinates = coordinates.fetch(:coordinates)
     @empty = true
     @ship = nil
     @hit = false
   end
 
   def hold(ship)
-    raise "This cell already holds a ship" if empty == false
+    raise "This cell already holds a ship" unless empty
     @empty = false 
     @ship = ship
   end
 
   def hit!
-    raise "This cell has already been hit" if hit == true
+    raise "This cell has already been hit" if hit
     @hit = true
+    @ship.hit_ship! unless empty
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,61 +2,72 @@ require './lib/cell'
 
 describe Cell do
 
-  let (:cell) { Cell.new(:coordinates => :A1) }
+  let (:cell) { Cell.new }
   let (:ship) { double :ship }
+  let (:water) { double :water, { :class => Water } } #{ :is_a? => true } }
 
   context 'initializing the cell' do
 
-    it 'raises an error if coordinates are not provided' do
-      expect(lambda { Cell.new }).to raise_error(ArgumentError)
+    # it 'raises an error if coordinates are not provided' do
+    #   expect(lambda { Cell.new }).to raise_error(ArgumentError)
+    # end
+
+    # it 'will initialize without a ship' do
+    #   expect(cell.empty).to be true
+    # end
+
+    it 'will initialize containing water' do
+      expect(cell.contents).to be(water)
     end
 
-    it 'will initialize without a ship' do
-      expect(cell.empty).to be true
-    end
+    # it 'will not be hit on initializing' do
+    #   expect(cell.hit).to be false
+    # end
 
-    it 'will not be hit on initializing' do
-      expect(cell.hit).to be false
-    end
-
-    it 'knows its co-ordinates when it has been placed on the board' do
-      expect(cell.coordinates).to be(:A1)
-    end
+    # it 'knows its co-ordinates when it has been placed on the board' do
+    #   expect(cell.coordinates).to be(:A1)
+    # end
 
   end
 
-  context 'holding a ship' do
+  # context 'holding a ship' do
 
-    it 'can hold a ship if it is empty' do
-      cell.hold(ship)
-      expect(cell.empty).to be false
-    end
+  #   it 'can hold a ship if it is empty' do
+  #     cell.hold(ship)
+  #     expect(cell.empty).to be false
+  #   end
 
-    it 'cannot hold a ship if it is not empty' do
-      ship2 = double(:ship)
-      cell.hold(ship)
-      expect { cell.hold(ship2) }.to raise_error(RuntimeError)
-    end
+  #   it 'cannot hold a ship if it is not empty' do
+  #     ship2 = double(:ship)
+  #     cell.hold(ship)
+  #     expect { cell.hold(ship2) }.to raise_error(RuntimeError)
+  #   end
 
-    it 'knows which ship it is holding' do
-      cell.hold(ship)
-      expect(cell.ship).to be(ship)
-    end
+  #   it 'knows which ship it is holding' do
+  #     cell.hold(ship)
+  #     expect(cell.ship).to be(ship)
+  #   end
 
-  end
+  # end
 
-  context 'being hit' do
+  # context 'being hit' do
 
-    it 'can be hit' do
-      cell.hit!
-      expect(cell.hit).to be true
-    end
+  #   it 'can be hit' do
+  #     cell.hit!
+  #     expect(cell.hit).to be true
+  #   end
 
-    it 'cannot be hit more than once' do
-      cell.hit!
-      expect { cell.hit! }.to raise_error(RuntimeError)
-    end
-  
-  end
+  #   it 'cannot be hit more than once' do
+  #     cell.hit!
+  #     expect { cell.hit! }.to raise_error(RuntimeError)
+  #   end
+
+  #   it 'will allow ship to be hit if inside cell' do
+  #     cell.hold(ship)
+  #     expect(ship).to receive(:hit_ship!)
+  #     cell.hit!
+  #   end
+
+  # end
 
 end
