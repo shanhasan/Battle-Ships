aed46dae3e47a61cb93bc5764947bb1401c5924e
ed <ejbyne@gmail.com>
2014-11-04 18:20:48 +0000

    Completed first version of Water class

--- spec/water_spec.rb
+++ spec/water_spec.rb
@@ -4,9 +4,12 @@ describe Water do
 
   let(:water) { Water.new }
 
-  xit 'can be hit' do
-    water.hit!
-    expect(water).to be_hit
+  it 'is not hit when initialized' do
+    expect(water).not_to be_hit
+  end
+
+  it 'can be hit' do
+    expect(water.hit!).to be_hit
   end
   
 end
