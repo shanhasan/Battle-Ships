267b4e274e8c70a046233af225d4b5af92dac07d
Richard Lake <ralake1985@gmail.com>
2014-11-06 19:15:08 +0000

    Add game.rb and game spec file

---
+++ spec/game_spec.rb
@@ -0,0 +1,55 @@
+require './lib/game.rb'
+
+describe Game do
+
+  let(:game)    { Game.new(player1) }
+  let(:player1) { double :Player }
+  let(:player2) { double :Player }
+  let(:ship)    { double :Ship }
+  let(:cell)    { double :Cell, {:hit? => true} }
+
+  it 'can have players' do
+    expect(game.players).to be_an_instance_of(Array)
+  end
+
+  it 'must have two players' do
+    expect(game.players).to be_an_instance_of(Array)
+    game.add_player(player2)
+    expect(game.players.count).to eq(2)
+  end
+
+  it 'cannot be played with more than two players' do
+    player3 = double("player")
+    game.add_player(player2)
+    expect { game.add_player(player3) }.to raise_error(RuntimeError) if game.players.count >= 2
+  end
+
+  it 'cannot be played with less than two players'
+
+  it 'alternates between the players to take turns' do
+    game.add_player(player2)
+    game.take_turn
+    expect(game.whos_turn).to eq(player2)
+  end
+
+  it 'allows player to take another turn if they hit a ship' do
+    expect(player1).to receive(:fire!)
+    player1.fire!
+    expect(ship).to receive(:hit?).and_return true
+    game.take_turn
+    expect(game.whos_turn).to eq(player1)
+  end
+
+
+  it 'must have two boards'
+
+  it 'must have 10 ships' do
+    10.times { game.add_ships(ship) }
+    expect(game.ships.count).to eq(10)
+  end
+
+  it 'knows when a players wins'
+    # if other players sunk ships array reaches count 5 a method is automatically called
+    # in the take turn function that checks if the player has won.
+
+end
