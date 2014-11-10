3010b9e547ebd36752656cd0b0fbd7848df68460
Richard Lake <ralake1985@gmail.com>
2014-11-10 16:00:40 +0000

    Add in five new class methods to create the fleet of ships

--- lib/ship.rb
+++ lib/ship.rb
@@ -22,8 +22,24 @@ class Ship
     @number_of_hits == @length
   end
 
-  def battleship
+  def self.battleship
     new 4
+  end
+
+  def self.aircraft_carrier
+    new 5
+  end
+
+  def self.destroyer
+    new 3
+  end
+
+  def self.patrol_boat
+    new 2
+  end
+
+  def self.submarine
+    new 3
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,29 +2,49 @@ require './lib/ship'
 
 describe Ship do
   
-  let (:ship) { Ship.new }
+  let (:ship) { Ship.new(3) }
 
-  xit 'is not hit when initializied' do
+  it 'is not hit when initializied' do
     expect(ship).not_to be_hit
   end
 
-  xit 'can be hit' do
+  it 'can be hit' do
     expect(ship.hit!).to be_hit
   end
 
-  xit 'is not until unless hit a number of times equal to its length' do
+  it 'is not sunk unless hit a number of times equal to its length' do
     2.times { ship.hit! }
     expect(ship).not_to be_sunk
   end
 
-  # it 'can be sunk' do
-  #   3.times { ship.hit! }
-  #   expect(ship).to be_sunk
-  # end
+  it 'knows when it is sunk' do
+    ship.length.times { ship.hit! }
+    expect(ship).to be_sunk
+  end
 
-  xit 'can create ships of different lengths' do
-    ship.battleship
-    expect(ship.length).to eq(4)
+  it 'can create a battleship' do
+    battleship = Ship.battleship
+    expect(battleship.length).to eq(4)
+  end
+
+  it 'can create an aircraft carrier' do
+    aircraft_carrier = Ship.aircraft_carrier
+    expect(aircraft_carrier.length).to eq(5)
+  end
+
+  it 'can create a destroyer' do
+    destroyer = Ship.destroyer
+    expect(destroyer.length).to eq(3)
+  end
+
+  it 'can create a patrol boat' do
+    patrol_boat = Ship.patrol_boat
+    expect(patrol_boat.length).to eq(2)
+  end
+
+  it 'can create a submarine' do
+    submarine = Ship.submarine
+    expect(submarine.length).to eq(3)
   end
 
 end
